b37365c605e0419f64af6292a84ff6f90043c0ed
YenHungLai <jacoblai1029@gmail.com>
2019-04-02 18:26:50 -0700

    added audio

--- src/component/Alarm.js
+++ src/component/Alarm.js
@@ -1,86 +1,106 @@
 // TODO:
-//  how to play audio or "alarm" the user
+//    how to stop alarm
+//
 
 import React from 'react';
 import MyButton from '../uiComponent/button'
 
 const style = {
-  fontSize: '15px',
-  fontFamily: 'Times New Roman',
-  padding: '5px',
+    fontSize: '15px',
+    fontFamily: 'Times New Roman',
+    padding: '5px',
 };
 
 class Alarm extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      hour: 0,
-      minutes: 0,
-      AMPM: 'AM',
-    };
-  }
-
-  // Does not work
-  play = () => {
-    this.audio.play();
-  }
-
-  handleClick = () => {
-    // Get value from select tag.
-    const inputHour = document.getElementById('myHour').value;
-    const inputMinutes = document.getElementById('myMinutes').value;
-    const inputAMPM = document.getElementById('myAMPM').value;
-    this.setState({ hour: inputHour });
-    this.setState({ minutes: inputMinutes });
-    this.setState({ AMPM: inputAMPM });
-    console.log('hour: '+this.state);
-    document.querySelector('#result').style.display = 'block'
-  }
-
-  render() {
-    let hours = [];
-    for(let i = 1; i <= 12; i++) {
-      if(i < 10)
-        hours.push(<option value={'0' + i}>{'0' + i}</option>);
-      else
-        hours.push(<option value={i}>{i}</option>);
+    constructor() {
+        super();
+        this.state = {
+            hour: 0,
+            minutes: 0,
+            AMPM: 'AM',
+        };
+        this.audio = new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
+        setInterval(this.checkTime, 1000)
     }
 
-    let minutes = [];
-    for(let i = 0; i <= 59; i++) {
-      if(i < 10)
-        minutes.push(<option value={'0' + i}>{'0' + i}</option>);
-      else
-        minutes.push(<option value={i}>{i}</option>);
+    // Does not work
+    play = () => {
+        this.audio.play();
     }
 
-    return (
-      <div className='alarmStyle'>
-        <span style={style}>Hour</span>
-        <select id="myHour" name="Hour">
-          {hours}
-        </select>
+    handleClick = () => {
+        // Get value from select tag.
+        const inputHour = document.getElementById('myHour').value;
+        const inputMinutes = document.getElementById('myMinutes').value;
+        const inputAMPM = document.getElementById('myAMPM').value;
+        this.setState({ hour: inputHour });
+        this.setState({ minutes: inputMinutes });
+        this.setState({ AMPM: inputAMPM });
+        console.log('hour: '+this.state);
+        document.querySelector('#result').style.display = 'block'
+    }
 
-        <span style={style}>Minutes</span>
-        <select id="myMinutes"name="Minutes">
-          {minutes}
-        </select>
+    checkTime = () => {
+        let currentHour = new Date().getHours();
+        let currenMinutes = new Date().getMinutes();
+        if(currentHour < 10)
+            currentHour = '0' + currentHour;
+        if(currenMinutes < 10)
+            currenMinutes = '0' + currenMinutes;
+        console.log(currentHour);
+        console.log(currenMinutes);
+        console.log(this.state);
+        // Why triple equal sign doesn't work???
+        if(this.state.hour == currentHour && this.state.minutes == currenMinutes) {
+            this.audio.play();
+            console.log('yes!!!');
+        }
+    }
 
-        <select id="myAMPM" name="AM/PM">
-          <option value="AM">AM</option>
-          <option value="PM">PM</option>
-        </select>
+    render() {
+        let hours = [];
+        for(let i = 1; i <= 12; i++) {
+            if(i < 10)
+            hours.push(<option value={'0' + i}>{'0' + i}</option>);
+            else
+            hours.push(<option value={i}>{i}</option>);
+        }
 
-        <br />
-        <br />
-        <MyButton action={this.handleClick} name='Set Alarm' />
+        let minutes = [];
+        for(let i = 0; i <= 59; i++) {
+            if(i < 10)
+            minutes.push(<option value={'0' + i}>{'0' + i}</option>);
+            else
+            minutes.push(<option value={i}>{i}</option>);
+        }
 
-        <h1 id='result' style={{display: 'none'}}>
-            You just set an alarm for {this.state.hour}:{this.state.minutes} {this.state.AMPM}
-        </h1>
-      </div>
-    );
-  }
+        return (
+            <div className='alarmStyle'>
+                <span style={style}>Hour</span>
+                <select id="myHour" name="Hour">
+                    {hours}
+                </select>
+
+                <span style={style}>Minutes</span>
+                <select id="myMinutes"name="Minutes">
+                    {minutes}
+                </select>
+
+                <select id="myAMPM" name="AM/PM">
+                    <option value="AM">AM</option>
+                    <option value="PM">PM</option>
+                </select>
+
+                <br />
+                <br />
+                <MyButton action={this.handleClick} name='Set Alarm' />
+
+                <h1 id='result' style={{display: 'none'}}>
+                    You just set an alarm for {this.state.hour}:{this.state.minutes} {this.state.AMPM}
+                </h1>
+            </div>
+        );
+    }
 }
 
 export default Alarm;
